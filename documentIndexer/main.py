c970d28c978ddcf783c4ccf7965d5ea37be5f09b
Carlos José Varela <carlosjvare@gmail.com>
2021-08-23 19:50:35 -0600

    Updated git ignore

--- documentIndexer/main.py
+++ documentIndexer/main.py
@@ -11,6 +11,7 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    print("Hola Roo")
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
