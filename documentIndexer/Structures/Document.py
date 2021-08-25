3b92385d7e2207cdfded91dde50fab0069b322f4
Joseph <jostenoriop@gmail.com>
2021-08-24 18:27:09 -0600

    Added basic data structures

---
+++ documentIndexer/Structures/Document.py
@@ -0,0 +1,11 @@
+class Document:
+
+    def __init__(self, address, size, norm):
+        """
+        :type address: str
+        :type size: int
+        :type norm: int
+        """
+        self.address = address
+        self.size = size
+        self.norm = norm
