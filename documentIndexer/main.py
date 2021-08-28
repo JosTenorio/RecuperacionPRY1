905fbd321252b33e6f9dd4bbc4e35f262239db7a
Joseph <jostenoriop@gmail.com>
2021-08-27 20:17:43 -0600

    Added terminal structure

--- documentIndexer/main.py
+++ documentIndexer/main.py
@@ -1,17 +1,55 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import cmd
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class Terminal(cmd.Cmd):
+    prompt = ">>"
+
+    intro = "-----------------------------------------------------------------------\n" \
+            "Bienvenido a la herramienta de consulta de archivos de texto \n" \
+            "La lista de comandos diponibles es: \n" \
+            ">>indizar  [Colección]  [Stopwords] [Indice]\n" \
+            ">>buscar  [Indice]  [Tipo]  [Prefijo]  [NumDocs]  [Consulta]\n" \
+            ">>mostrar  [Indice]  [Tipo]  [Dato]\n" \
+            ">>salir\n" \
+            "Con '>>help' o '>>?' puede consultar esta lista o algun comando en especifico\n" \
+            "-----------------------------------------------------------------------"
+
+    def do_indizar(self, person):
+        """indizar  [Colección]  [Stopwords] [Indice]
+        Greet the named person"""
+        print("Comando de indizacion")
+
+    def do_buscar(self, person):
+        """buscar  [Indice]  [Tipo]  [Prefijo]  [NumDocs]  [Consulta]
+        Greet the named person"""
+        print("Comando de busquéda")
+
+    def do_mostrar(self, person):
+        """mostrar  [Indice]  [Tipo]  [Dato]
+        Greet the named person"""
+        print("Comando de inspección")
+
+    def do_salir(self, line):
+        """salir
+        Termina la ejecución del programa"""
+        return True
+
+    def do_help(self, line):
+        if len(line.split()) > 0:
+            cmd.Cmd.do_help(self, line)
+        else:
+            print("La lista de comandos diponibles es: \n" \
+                  ">>indizar  [Colección]  [Stopwords] [Indice]\n" \
+                  ">>buscar  [Indice]  [Tipo]  [Prefijo]  [NumDocs]  [Consulta]\n" \
+                  ">>mostrar  [Indice]  [Tipo]  [Dato]\n" \
+                  "Con '>>help' o '>>?' puede consultar esta lista o algun comando en especifico\n")
+
+    def do_EOF(self, line):
+        return True
+
+    def postloop(self):
+        print("Terminando ejecución")
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print("Hola Roo")
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    Terminal().cmdloop()
